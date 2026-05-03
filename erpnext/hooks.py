d5416a5179eca892330edef36d932a482e4475ee
fardanylabib <fardanylabib@gmail.com>
2026-05-03 09:26:29 +0700

    Update erpnext.json, hooks.py, and erpnext-logo-title.png

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -114,7 +114,7 @@ website_generators = ["BOM", "Sales Partner"]
 
 website_context = {
 	"favicon": "/assets/erpnext/images/erpnext-favicon.svg",
-	"splash_image": "/assets/erpnext/images/erpnext-logo.svg",
+	"splash_image": "/assets/erpnext/images/erpnext-logo-title.png",
 }
 
 # nosemgrep
