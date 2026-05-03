dd7fa5dbd2a7d08be57631808fbceeafa0394a89
fardanylabib <fardanylabib@gmail.com>
2026-05-03 10:35:25 +0700

    Update hooks.py

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -7,13 +7,13 @@ app_color = "#e74c3c"
 app_email = "hello@frappe.io"
 app_license = "GNU General Public License (v3)"
 source_link = "https://github.com/frappe/erpnext"
-app_logo_url = "/assets/erpnext/images/erpnext-logo-title.png"
+app_logo_url = "/assets/erpnext/images/erpnext-logo.svg"
 app_home = "/desk"
 
 add_to_apps_screen = [
 	{
 		"name": app_name,
-		"logo": "/assets/erpnext/images/erpnext-logo.svg",
+		"logo": "/assets/erpnext/images/erpnext-logo-title.png",
 		"title": app_title,
 		"route": app_home,
 		"has_permission": "erpnext.check_app_permission",
